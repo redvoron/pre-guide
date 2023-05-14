21f4078c9108fad338ac20f104203f4a1b303490
Roman Novikov <rno@tassta.com>
2023-05-14 18:37:00 +0400

    Add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
 import { AppProps } from 'next/app';
-
 import '../styles/global.css';
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Script
+      id="gtag-script"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-P5R4N7VV20"
+    />
+    <Script id="gtag-layer">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-P5R4N7VV20');
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
